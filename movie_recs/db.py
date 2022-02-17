3f09b92d4946b0d17e6132fedd67aeeb7ea3b9d9
John Allen Carruth <johnallen.carruth@gmail.com>
2022-02-16 22:12:54 -0600

    Add database connection code

---
+++ movie_recs/db.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+from flask import current_app, g
+
+
+def get_db() -> MongoClient:
+    """Provides access to the database"""
+
+    if "db" not in g:
+        username = current_app.config["DB_USER"]
+        password = current_app.config["DB_PASSWORD"]
+        host = current_app.config["DB_HOST"]
+        port = current_app.config["DB_PORT"]
+        uri = f"mongodb://{username}:{password}@{host}:{port}"
+        g.db = MongoClient(uri)
+
+    return g.db
+
+
+def close_db():
+    """ Closes the database connection """
+    db: MongoClient = g.pop("db", None)
+
+    if db is not None:
+        db.close()
