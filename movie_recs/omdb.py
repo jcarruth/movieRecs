8af4081dcc7080c41b8264e77f8ab5d5cefd320a
John Allen Carruth <johnallen.carruth@gmail.com>
2022-02-17 17:00:55 -0600

    Provide access to omdb

---
+++ movie_recs/omdb.py
@@ -0,0 +1,27 @@
+""" Provides access to the Open Movie Database """
+import requests
+
+from flask import current_app
+
+def get_movie_data(movie_title: str) -> dict:
+    """ Get data for a movie title """
+
+    params = {
+        "apikey": current_app.config["OMDB_API_KEY"],
+        "t": movie_title,
+        "plot": "short"
+    }
+
+    response = requests.get("http://www.omdbapi.com/", params=params)
+
+    if "Response" not in response.json() or not response.json()["Response"]:
+        raise Exception("")
+
+    synopsis = response.json()["Plot"]
+
+    params["plot"] = "full"
+    response = requests.get("http://www.omdbapi.com/", params=params)
+    movie_data = response.json()
+    movie_data["Synopsis"] = synopsis
+
+    return movie_data
