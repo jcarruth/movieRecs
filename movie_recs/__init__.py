24eb66e97c08c501f8f562fe7d2042ec1c01146f
John Allen Carruth <johnallen.carruth@gmail.com>
2022-02-17 07:31:56 -0600

    Add default db config to app

--- movie_recs/__init__.py
+++ movie_recs/__init__.py
@@ -5,6 +5,14 @@ def create_app():
     """ create and configure the flask app """
     app = Flask(__name__)
 
+    app.config.from_mapping(
+        DB_USER="root",
+        DB_PASSWORD="example",
+        DB_HOST="mongo",
+        DB_PORT=27017,
+        SECRET_KEY='dev',
+    )
+
     @app.route("/test")
     def test():
         return "test"
